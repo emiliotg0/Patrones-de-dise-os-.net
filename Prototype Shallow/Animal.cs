e6a1ea214e92a57678a339621618ef8a3eaf7f7c
emiliotg0 <41089735+emiliotg0@users.noreply.github.com>
2020-03-30 00:03:44 -0400

    Add files via upload

---
+++ Prototype Shallow/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype_Shalloq
+{
+    public class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+
+        public string nombre { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype Shallow/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Prototype_Shalloq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //objeto primario
+            Animal animal = new Animal() { nombre = "Perro Firulais", Patas = 4 };
+            //objeto clonado
+            Animal animalclon = animal.Clone() as Animal;
+            animalclon.nombre = "Gato misu";
+            animalclon.Patas = 3;
+
+            Console.WriteLine(animal.nombre+" Tiene:"+animal.Patas+" Patas");
+            Console.WriteLine(animalclon.nombre + " Tiene:" + animalclon.Patas + " Patas");
+        }
+    }
+}
