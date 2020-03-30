ad8c7703e0b3395d11c5f4655bbe1c1c9b5737ab
emiliotg0 <41089735+emiliotg0@users.noreply.github.com>
2020-03-29 23:42:03 -0400

    Add files via upload

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance = null;
+        public string mensaje = "";
+        protected Singleton() { 
+        mensaje="Hola mundo"}
+
+        public static Singleton Instance
+        {
+            get 
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
+        }
+    }
+}
